63d23e4ce6799da5d5a79710af802468d0745b30
yvesgurcan <gurcan.yves+github@gmail.com>
2020-08-26 11:40:08 -0700

    Apply texture

--- src/components/DomeWalls.js
+++ src/components/DomeWalls.js
@@ -1,36 +1,36 @@
-import React, { useRef } from 'react';
+import React, { useMemo } from 'react';
 import * as Three from 'three';
 import { ThreeBSP } from 'three-js-csg-es6';
 import { Mesh, CylinderGeometry } from 'three';
 
 export default () => {
-    const elementReference = useRef();
+    const domeWallMesh = useMemo(() => {
+        // build geometry
+        const outerCylinder = new Mesh(new CylinderGeometry(6, 6, 12, 60, 4));
+        const innerCylinder = new Mesh(new CylinderGeometry(5, 5, 12, 60, 4));
+        const icBSP = new ThreeBSP(innerCylinder);
+        const ocBSP = new ThreeBSP(outerCylinder);
 
-    const outerCylinder = new Mesh(new CylinderGeometry(6, 6, 12, 60, 4));
-    const innerCylinder = new Mesh(new CylinderGeometry(5, 5, 12, 60, 4));
-    const icBSP = new ThreeBSP(innerCylinder);
-    const ocBSP = new ThreeBSP(outerCylinder);
+        // carve outer cylinder with inner cylinder
+        const carvedCylinder = ocBSP.subtract(icBSP);
 
-    // carve outer cylinder with inner cylinder
-    const carvedCylinder = ocBSP.subtract(icBSP);
-    const domeWallMesh = carvedCylinder.toMesh();
-    domeWallMesh.material = new Three.MeshPhongMaterial({
-        color: 0xdddddd,
-        specular: 0x1a1a1a,
-        shininess: 30,
-        shading: Three.FlatShading
+        // create mesh
+        const mesh = carvedCylinder.toMesh();
+        mesh.material = new Three.MeshPhongMaterial({
+            color: 0xdddddd,
+            specular: 0x1a1a1a,
+            shininess: 30,
+            shading: Three.FlatShading
+        });
+
+        // apply texture to mesh
+        const texture = new Three.TextureLoader().load(
+            'https://raw.githubusercontent.com/yvesgurcan/3d-dome/master/public/metal.jpg'
+        );
+        mesh.material.map = texture;
+
+        return mesh;
     });
-    return (
-        <>
-            <mesh position={[0, 3, 0]} ref={elementReference}>
-                <meshLambertMaterial
-                    attach="material"
-                    color="rgb(0, 168, 224)"
-                    roughness={1}
-                    metalness={0.3}
-                />
-            </mesh>
-            <primitive object={domeWallMesh} />
-        </>
-    );
+
+    return <primitive object={domeWallMesh} />;
 };
