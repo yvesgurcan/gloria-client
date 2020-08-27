74e6c704c742050b4f59542088ff2cf90a4c4dad
yvesgurcan <gurcan.yves+github@gmail.com>
2020-08-27 16:27:44 -0700

    Listen to mobile device moving

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -6,20 +6,125 @@ import { DeviceOrientationControls } from 'three/examples/jsm/controls/DeviceOri
 
 extend({ OrbitControls, DeviceOrientationControls });
 
+const zee = new Three.Vector3(0, 0, 1);
+const euler = new Three.Euler();
+const q0 = new Three.Quaternion();
+const q1 = new Three.Quaternion(-Math.sqrt(0.5), 0, 0, Math.sqrt(0.5));
+
+function setObjectQuaternion(quaternion, alpha, beta, gamma, orient) {
+    // 'ZXY' for the device, but 'YXZ' for us
+    euler.set(beta, alpha, -gamma, 'YXZ');
+
+    // Orient the device
+    quaternion.setFromEuler(euler);
+
+    // camera looks out the back of the device, not the top
+    quaternion.multiply(q1);
+
+    // adjust for screen orientation
+    quaternion.multiply(q0.setFromAxisAngle(zee, -orient));
+}
+
+function Quat2Angle(x, y, z, w) {
+    let pitch, roll, yaw;
+
+    const test = x * y + z * w;
+    // singularity at north pole
+    if (test > 0.499) {
+        yaw = Math.atan2(x, w) * 2;
+        pitch = Math.PI / 2;
+        roll = 0;
+
+        return new Three.Vector3(pitch, roll, yaw);
+    }
+
+    // singularity at south pole
+    if (test < -0.499) {
+        yaw = -2 * Math.atan2(x, w);
+        pitch = -Math.PI / 2;
+        roll = 0;
+        return new Three.Vector3(pitch, roll, yaw);
+    }
+
+    const sqx = x * x;
+    const sqy = y * y;
+    const sqz = z * z;
+
+    yaw = Math.atan2(2 * y * w - 2 * x * z, 1 - 2 * sqy - 2 * sqz);
+    pitch = Math.asin(2 * test);
+    roll = Math.atan2(2 * x * w - 2 * y * z, 1 - 2 * sqx - 2 * sqz);
+
+    return new Three.Vector3(pitch, roll, yaw);
+}
+
 export default ({
     autoRotate = false,
     delayRotation,
     target = [3, 0.08, 0],
     ...props
 }) => {
-    const elementReference = useRef();
+    const orbitControlsReference = useRef();
     const { camera, gl } = useThree();
 
-    window.addEventListener(
-        'deviceorientation',
-        event => console.log('deviceorientation changed', event),
-        true
-    );
+    useEffect(() => {
+        orbitControlsReference.current.update = () => {
+            const oC = orbitControlsReference.current;
+            // Z
+            const alpha = oC.deviceOrientation.alpha
+                ? Three.Math.degToRad(oC.deviceOrientation.alpha)
+                : 0;
+
+            // X'
+            const beta = oC.deviceOrientation.beta
+                ? Three.Math.degToRad(oC.deviceOrientation.beta)
+                : 0;
+
+            // Y''
+            const gamma = oC.deviceOrientation.gamma
+                ? Three.Math.degToRad(oC.deviceOrientation.gamma)
+                : 0;
+
+            // O
+            const orient = oC.screenOrientation
+                ? Three.Math.degToRad(oC.screenOrientation)
+                : 0;
+
+            const currentQ = new Three.Quaternion().copy(oC.object.quaternion);
+
+            setObjectQuaternion(currentQ, alpha, beta, gamma, orient);
+            const currentAngle = Quat2Angle(
+                currentQ.x,
+                currentQ.y,
+                currentQ.z,
+                currentQ.w
+            );
+
+            // currentAngle.z = left - right
+            oC.rotateLeft((lastGamma - currentAngle.z) / 2);
+            lastGamma = currentAngle.z;
+
+            // currentAngle.y = up - down
+            oC.rotateUp(lastBeta - currentAngle.y);
+            lastBeta = currentAngle.y;
+        };
+
+        function onDeviceOrientationChangeEvent(event) {
+            orbitControlsReference.current.deviceOrientation = event;
+        }
+
+        window.addEventListener(
+            'deviceorientation',
+            onDeviceOrientationChangeEvent,
+            false
+        );
+
+        return () => {
+            window.removeEventListener(
+                'deviceorientation',
+                onDeviceOrientationChangeEvent
+            );
+        };
+    }, []);
 
     useFrame(() => {
         if (delayRotation) {
@@ -27,7 +132,7 @@ export default ({
         }
 
         // We need to update frames when the camera is rotating without user input
-        elementReference.current.update();
+        orbitControlsReference.current.update();
     });
 
     return (
@@ -36,7 +141,7 @@ export default ({
             enableDamping
             enablePan
             keyPanSpeed={40}
-            ref={elementReference}
+            ref={orbitControlsReference}
             args={[camera, gl.domElement]}
             rotateSpeed={0.5}
             dampingFactor={1}
