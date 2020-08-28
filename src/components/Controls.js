309f58d3ae3d57a7188b9ad240ee5ea5bd9567c4
yvesgurcan <gurcan.yves+github@gmail.com>
2020-08-27 19:36:05 -0700

    Remove mobile stuff from Controls

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,88 +1,17 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef } from 'react';
 import { useFrame, useThree, extend } from 'react-three-fiber';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { DeviceOrientationControls } from 'three/examples/jsm/controls/DeviceOrientationControls.js';
 
-extend({ OrbitControls, DeviceOrientationControls });
-
-const simulateDeviceOrientation = (orientation, onDeviceOrientation) => {
-    setInterval(
-        () =>
-            onDeviceOrientation({
-                alpha: orientation[0], // up and down
-                beta: orientation[1], // forward and backward
-                gamma: orientation[2] // left and right
-            }),
-        50
-    );
-};
+extend({ OrbitControls });
 
 export default ({
     autoRotate = false,
     delayRotation,
-    target: targetProp = [3, 0.08, 0],
+    target = [3, 0.08, 0],
     ...props
 }) => {
-    const [manualControls, setManualControls] = useState(true);
-    const [target, setTarget] = useState(targetProp);
-    const targetRef = useRef(targetProp);
     const orbitControlsReference = useRef();
-    const controls = useRef();
     const { camera, gl } = useThree();
-
-    const onDeviceOrientation = event => {
-        // deactivate orbit controls
-        if (manualControls) {
-            // setManualControls(false);
-        }
-
-        const { alpha, beta, gamma } = event;
-
-        var rotateDegrees = alpha;
-        // gamma: left to right
-        var leftToRight = gamma;
-        // beta: front back motion
-        var frontToBack = beta;
-
-        const newTarget = [
-            Math.max(-15, Math.min(15, targetRef.current[0] - alpha / 360)), // up and down
-            targetRef.current[1], //targetRef.current[1] - beta / 180, // forward and backward
-            targetRef.current[2] //targetRef.current[2] - gamma / 90 // left and right
-        ];
-        console.log('target', target);
-        console.log('newTarget', newTarget);
-        console.log({ alpha, beta, gamma });
-        console.log('---');
-
-        targetRef.current = newTarget;
-        setTarget(newTarget);
-    };
-
-    /*
-    useEffect(() => {
-        simulateDeviceOrientation(
-            [
-                // Math.random() * 360,
-                50, // Math.random() * 180 - 180,
-                Math.random() * 90 - 90
-            ],
-            onDeviceOrientation
-        );
-
-        window.addEventListener(
-            'deviceorientation',
-            onDeviceOrientation,
-            false
-        );
-
-        return () => {
-            window.removeEventListener(
-                'deviceorientation',
-                onDeviceOrientation
-            );
-        };
-    }, []);
-    */
 
     useFrame(() => {
         if (delayRotation) {
@@ -95,7 +24,7 @@ export default ({
 
     return (
         <orbitControls
-            enabled={manualControls}
+            enabled
             enableDamping
             enablePan
             keyPanSpeed={40}
