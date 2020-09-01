d6f78dbcd7903626e96f5488cb8cae6f0581e371
yvesgurcan <gurcan.yves+github@gmail.com>
2020-09-01 13:50:22 -0700

    Move 3D assets to S3

--- src/components/TriforceARViewer.js
+++ src/components/TriforceARViewer.js
@@ -4,7 +4,7 @@ export default () => {
     return (
         <model-viewer
             src="https://raw.githubusercontent.com/yvesgurcan/3d-dome/master/public/triforce2/scene.gltf"
-            ios-src="https://raw.githubusercontent.com/yvesgurcan/3d-dome/master/public/triforce.usdz"
+            ios-src="https://gloria-3d-assets.s3-us-west-2.amazonaws.com/triforce.usdz"
             ar-modes="webxr scene-viewer quick-look"
             ar
             auto-rotate
