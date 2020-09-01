e2abe8838283e9691cc371c3bd34fceb87c65007
yvesgurcan <gurcan.yves+github@gmail.com>
2020-09-01 14:22:39 -0700

    Modify link to model

--- src/components/TriforceARViewer.js
+++ src/components/TriforceARViewer.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 export default () => {
     return (
-        <model-viewer
-            src="https://raw.githubusercontent.com/yvesgurcan/3d-dome/master/public/triforce2/scene.gltf"
-            ios-src="https://gloria-3d-assets.s3-us-west-2.amazonaws.com/triforce.usdz"
-            ar-modes="webxr scene-viewer quick-look"
-            ar
-            auto-rotate
-            camera-controls
-        />
+        <a href="https://gloria-3d-assets.s3-us-west-2.amazonaws.com/triforce.usdz">
+            <model-viewer
+                src="https://raw.githubusercontent.com/yvesgurcan/3d-dome/master/public/triforce2/scene.gltf"
+                ar-modes="webxr scene-viewer quick-look"
+                ar
+                auto-rotate
+                camera-controls
+            />
+        </a>
     );
 };
