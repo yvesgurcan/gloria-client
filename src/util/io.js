6f366551eb794ac65ff0d30a08360bd5de30fc14
yvesgurcan <gurcan.yves+github@gmail.com>
2020-09-02 17:22:12 -0700

    HTTPS

--- src/util/io.js
+++ src/util/io.js
@@ -1,16 +1,10 @@
 import socketIOClient from 'socket.io-client';
 
 const WEBSOCKET_SERVER_URL =
-    location.hostname === 'localhost'
+    /*location.hostname === 'localhost'
         ? 'http://localhost:3000'
-        : 'http://54.189.2.73:3000/';
+        :*/ 'https://54.189.2.73/';
 
 const socket = socketIOClient(WEBSOCKET_SERVER_URL);
 
 export default socket;
-
-/*
-socket.on('connect', console.log('oi'));
-socket.on('event', function (data) {});
-socket.on('disconnect', function () {});
-*/
