d130aa745fc4b4812d6010d17d598120370fd1c4
Brandon Stump <brandon.stump@gmail.com>
2020-09-07 11:33:38 -0700

    Updated skybox with new image from TF7.

--- src/components/Skybox.js
+++ src/components/Skybox.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import { CubeTextureLoader } from 'three';
+import { TextureLoader, WebGLCubeRenderTarget } from 'three';
 import { useThree } from 'react-three-fiber';
 
 export default () => {
-    const { scene } = useThree();
-    const loader = new CubeTextureLoader();
-    // The CubeTextureLoader load method takes an array of URLs representing all 6 sides of the cube.
-    const texture = loader.load([
-        'public/space-skybox/1.jpg',
-        'public/space-skybox/2.jpg',
-        'public/space-skybox/3.jpg',
-        'public/space-skybox/4.jpg',
-        'public/space-skybox/5.jpg',
-        'public/space-skybox/6.jpg'
-    ]);
-    // Set the scene background property to the resulting texture.
-    scene.background = texture;
+    const { gl, scene } = useThree();
+    const loader = new TextureLoader();
+    const texture = loader.load(
+      'public/tf7/kloppenheim_06.jpg',
+      () => {
+        const rt = new WebGLCubeRenderTarget(texture.image.height);
+        rt.fromEquirectangularTexture(gl, texture);
+        scene.background = rt;
+      });
     return null;
 };
