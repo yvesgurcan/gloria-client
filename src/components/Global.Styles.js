55f412cb43f7db9d31e10c38ce77ec39089191c7
yvesgurcan <gurcan.yves+github@gmail.com>
2020-09-11 09:09:22 -0700

    Indententation

--- src/components/Global.Styles.js
+++ src/components/Global.Styles.js
@@ -1,25 +1,25 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-body {
-    font-family: sans-serif;
-    margin: 0;
-}
-
-html,
-body,
-#root {
-    width: 100%;
-    height: 100%;
-}
-
-a {
-    text-decoration: none;
-    color: white;
-    font-weight: bold;
-
-    :hover {
-        color: black;
+    body {
+        font-family: sans-serif;
+        margin: 0;
     }
-}
+
+    html,
+    body,
+    #root {
+        width: 100%;
+        height: 100%;
+    }
+
+    a {
+        text-decoration: none;
+        color: white;
+        font-weight: bold;
+
+        :hover {
+            color: black;
+        }
+    }
 `;
